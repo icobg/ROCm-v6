51d3b1210ab9eb8753b639b9afac0085c59b64dc
Condor <condor@stz-bg.com>
2025-01-11 10:30:14 +0200

    Update 44.rocthrust.sh

--- 44.rocthrust.sh
+++ 44.rocthrust.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 set -e
-
-cd $ROCM_REL_DIR
-wget https://github.com/ROCm/rocThrust/archive/rocm-$PKGVER.tar.gz
-tar xf rocThrust-$LDIR.tar.gz
-rm -rf $ROCM_BUILD_DIR/rocthrust
-mkdir -p $ROCM_BUILD_DIR/rocthrust
-cd $ROCM_BUILD_DIR/rocthrust
-
-DEST=$OUTPUT/package-rocthrust
 PRGNAM=rocThrust
+cd $ROCM_REL_DIR
+wget https://github.com/ROCm/$PRGNAM/archive/rocm-$PKGVER.tar.gz
+tar xf $PRGNAM-$LDIR.tar.gz
+rm -rf $ROCM_BUILD_DIR/$PRGNAM
+mkdir -p $ROCM_BUILD_DIR/$PRGNAM
+cd $ROCM_BUILD_DIR/$PRGNAM
+
+DEST=$OUTPUT/package-$PRGNAM
+
 NUMJOBS=${NUMJOBS:-" -j$(expr $(nproc) + 1) "}
 BUILD=1
 rm -rf $DEST
@@ -23,7 +23,7 @@ cmake \
     -D CMAKE_CXX_COMPILER=${ROCM_INSTALL_DIR}/bin/hipcc \
     -D CMAKE_CXX_FLAGS="${CXXFLAGS} -fcf-protection=none" \
     -D CMAKE_INSTALL_PREFIX=${ROCM_INSTALL_DIR} \
-    $ROCM_REL_DIR/rocThrust-$LDIR
+    $ROCM_REL_DIR/$PRGNAM-$LDIR
 
 cmake --build . $NUMJOBS
 DESTDIR=$DEST cmake --install . --strip
@@ -54,4 +54,3 @@ cd $DEST
 makepkg -l y -c n $OUTPUT/$PRGNAM-$PKGVER-$ARCH-${BUILD}$TAG.txz
 
 popd
-
