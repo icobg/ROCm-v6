ca0018642fb4be817db42417114f367d4c3b5977
Condor <condor@stz-bg.com>
2025-08-10 17:19:00 +0300

    Update version

--- 100.amdprolibs.sh
+++ 100.amdprolibs.sh
@@ -21,9 +21,9 @@ TAG=condor
 
 major=25.10
 major_short=25.10
-minor=2165406
+minor=2194696
 ubuntu_ver=24.04
-repo_folder_ver=6.4.1
+repo_folder_ver=6.4.3
 amf_ver=1.4.37
 tmp=/tmp/condor/amdgpu
 src=/tmp/gpu
