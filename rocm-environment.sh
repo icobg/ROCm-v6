31487c7537466eda2840d1e9a6089fb744697b2c
Condor <condor@stz-bg.com>
2025-11-01 13:23:26 +0200

    Based on Tensile that is supported GPU

--- rocm-environment.sh
+++ rocm-environment.sh
@@ -12,7 +12,7 @@ export ROCM_PKGTYPE=TGZ
 export ROCM_REL_DIR=/usr/local/src/rocm/release
 export ROCM_BUILD_DIR=/usr/local/src/rocm/rocm-build/build
 # Uncomment line bellow only if you will build all kernels, for home users autodetected is better
-#export AMDGPU_TARGETS="gfx908,gfx90a,gfx942,gfx950,gfx1030,gfx1100,gfx1101,gfx1200,gfx1201"
+#export AMDGPU_TARGETS="gfx803,gfx900,gfx906,gfx908,gfx90a,gfx942,gfx950,gfx1010,gfx1011,gfx1012,gfx1030,gfx1031,gfx1032,gfx1034,gfx1035,gfx1100,gfx1101,gfx1102,gfx1103,gfx1150,gfx1151,gfx1200,gfx1201"
 export LIBRARY_PATH=$ROCM_INSTALL_DIR/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROCM_INSTALL_DIR/lib:$LD_LIBRARY_PATH
 export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$CMAKE_DIR/bin:$PATH
