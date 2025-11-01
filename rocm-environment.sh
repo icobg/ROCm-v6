6e7beb69971843946b56ba776d02c80d6dafbce2
Condor <condor@stz-bg.com>
2025-11-01 09:43:23 +0200

    Removed unsupported devices

--- rocm-environment.sh
+++ rocm-environment.sh
@@ -12,7 +12,7 @@ export ROCM_PKGTYPE=TGZ
 export ROCM_REL_DIR=/usr/local/src/rocm/release
 export ROCM_BUILD_DIR=/usr/local/src/rocm/rocm-build/build
 # Uncomment line bellow only if you will build all kernels, for home users autodetected is better
-#export AMDGPU_TARGETS="gfx803,gfx900,gfx906,gfx908,gfx90a,gfx942,gfx950,gfx1030,gfx1100,gfx1101,gfx1102,gfx1151,gfx1200,gfx1201"
+#export AMDGPU_TARGETS="gfx908,gfx90a,gfx942,gfx950,gfx1030,gfx1100,gfx1101,gfx1200,gfx1201"
 export LIBRARY_PATH=$ROCM_INSTALL_DIR/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$ROCM_INSTALL_DIR/lib:$LD_LIBRARY_PATH
 export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$CMAKE_DIR/bin:$PATH
